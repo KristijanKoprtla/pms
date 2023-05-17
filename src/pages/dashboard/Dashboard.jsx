41aed869fc69e2a8a08c4ace151117b87e705038
BombMarley <kile146@gmail.com>
2023-05-17 20:03:03 +0200

    Add ProjectFilter component

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,5 +1,7 @@
 import ProjectList from '../../components/ProjectList'
 import { useCollection } from '../../hooks/useCollection'
+import { useState } from 'react'
+import ProjectFilter from './ProjectFilter'
 
 // styles
 import './Dashboard.css'
@@ -7,11 +9,19 @@ import './Dashboard.css'
 
 export default function Dashboard() {
   const { documents, error } = useCollection('projects')
+  const [currentFilter, setCurrentFilter] = useState('all')
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter)
+  }
 
   return (
     <div>
       <h2 className="page-title">Dashboard</h2>
       {error && <p className='error'>{error}</p>}
+      {documents && (
+        <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />
+      )}
       {documents && <ProjectList projects={documents}/>}
     </div>
   )
